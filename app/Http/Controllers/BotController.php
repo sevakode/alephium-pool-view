53d3b8cc409d237d6057b997e3a1dd852d003055
sevakode <sevakode@gmail.com>
2022-01-23 04:09:00 +0300

    update

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\TelegramSender;
+use App\Models\Farmer;
+use App\Models\Share;
 use Illuminate\Http\Request;
 
 class BotController extends Controller
@@ -10,7 +12,27 @@ class BotController extends Controller
     //
     public function init(Request $request){
         $telegram=new TelegramSender();
-        $resp=$telegram->sendMessage('689839038',$request->all());
-        dd($resp);
+        $farmer=Farmer::where('telegram_id',$request->from->id)->first();
+        if($farmer){
+          if($request->message->text=="/stats"){
+              $stats=$farmer->stats();
+              $telegram->sendMessage($request->from->id,$stats);
+          }
+          else{
+              $telegram->sendMessage($request->from->id,"Я тебя не понимаю");
+          }
+        }
+        else{
+            $shares=Share::where('worker',$request->message->text)->first();
+            if($shares){
+                $farmer=new Farmer ();
+                $farmer->telegram_id=$request->from->id;
+                $farmer->address=$request->message->text;
+                $farmer->save();
+            }
+            else{
+               $telegram->sendMessage($request->from->id,"Введите номер кошелька");
+            }
+        }
     }
 }
