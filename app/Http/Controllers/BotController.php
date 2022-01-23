521ae4e48d88672cdfb4e8b6257a4cd83c5a1e84
sevakode <sevakode@gmail.com>
2022-01-23 05:39:07 +0300

    update

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -12,26 +12,33 @@ class BotController extends Controller
     //
     public function init(Request $request){
         $telegram=new TelegramSender();
-        $farmer=Farmer::where('telegram_id',$request->message['from']['id'])->get()->first();
+
+        $message=$request->get('message');
+        $telegram->sendMessage($message['from']['id'],$message);
+
+        $farmer=Farmer::where('telegram_id',$message['from']['id'])->get()->first();
+
         if($farmer){
-          if($request->message->text=="/stats"){
+          if($message['text']=="/stats"){
               $stats=$farmer->stats();
-              $telegram->sendMessage($request->message['from']['id'],$stats);
+              $telegram->sendMessage($message['from']['id'],$stats);
           }
           else{
-              $telegram->sendMessage($request->message['from']['id'],"Я тебя не понимаю");
+              $telegram->sendMessage($message['from']['id'],"Я тебя не понимаю");
           }
         }
         else{
-            $shares=Share::where('worker',$request->message['text'])->first();
+            $shares=Share::where('worker',$message['text'])->first();
             if($shares){
                 $farmer=new Farmer ();
-                $farmer->telegram_id=$request->message['from']['id'];
-                $farmer->address=$request->message['text'];
+                $farmer->telegram_id=$message['from']['id'];
+                $farmer->address=$message['text'];
                 $farmer->save();
+                $telegram->sendMessage($message['from']['id'],"Кошелек добавлен");
+
             }
             else{
-               $telegram->sendMessage($request->message['from']['id'],"Введите номер кошелька");
+               $telegram->sendMessage($message['from']['id'],"Введите адрес кошелька");
             }
         }
     }
