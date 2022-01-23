46bee72bc0a5eca30ec2fc91a769c7ca8cbc5f6c
sevakode <sevakode@gmail.com>
2022-01-23 03:14:04 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 Route::get('/pool_rev', [\App\Http\Controllers\BlockController::class, 'forDay']);
 Route::get('/shares_day', [\App\Http\Controllers\ShareController::class, 'forDayPool']);
 Route::get('/shares_hour', [\App\Http\Controllers\ShareController::class, 'forHourPool']);
-Route::get('/bot', [\App\Http\Controllers\BotController::class, 'init']);
+Route::match(['get', 'post'], '/bot', [\App\Http\Controllers\BotController::class, 'init']);
