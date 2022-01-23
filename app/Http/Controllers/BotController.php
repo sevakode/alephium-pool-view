d95209c9b88b5ec6adef10bba52bcf20da1ca1dd
sevakode <sevakode@gmail.com>
2022-01-23 06:46:33 +0300

    update

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -15,42 +15,71 @@ class BotController extends Controller
 
         $message=$request->get('message');
 //        $telegram->sendMessage($message['from']['id'],$message);
+        if(in_array(['689839038'],$message['from']['id'])){
+            $farmer=Farmer::where('address',$message['text'])->get()->first();
 
-        $farmer=Farmer::where('telegram_id',$message['from']['id'])->get()->first();
+            if($farmer){
+                $stats=$farmer->stats();
 
-        if($farmer){
-          if($message['text']=="/stats"){
-              $stats=$farmer->stats();
+                if($stats['day']>1000){
+                    $stats['day']=$stats['day']/1000;
+                    $stats['hour']=$stats['hour']/1000;
 
-              if($stats['day']>1000){
-                  $stats['day']=$stats['day']/1000;
-                  $stats['hour']=$stats['hour']/1000;
+                    $text="Хешрейт за 24 часа: ".$stats['day']."GH/s\nХешрейт за 1 час: ".$stats['hour']."GH/s";
+                }
+                else{
+                    $text="Хешрейт за 24 часа: ".$stats['day']."Mh/s\nХешрейт за 1 час: ".$stats['hour']."Mh/s";
 
-                  $text="Хешрейт за 24 часа: ".$stats['day']."GH/s\nХешрейт за 1 час: ".$stats['hour']."GH/s";
-              }
-              else{
-                  $text="Хешрейт за 24 часа: ".$stats['day']."Mh/s\nХешрейт за 1 час: ".$stats['hour']."Mh/s";
-
-              }
-              $telegram->sendMessage($message['from']['id'],$text);
-          }
-          else{
-              $telegram->sendMessage($message['from']['id'],"Я тебя не понимаю");
-          }
-        }
-        else{
-            $shares=Share::where('worker',$message['text'])->first();
-            if($shares){
-                $farmer=new Farmer ();
-                $farmer->telegram_id=$message['from']['id'];
-                $farmer->address=$message['text'];
-                $farmer->save();
-                $telegram->sendMessage($message['from']['id'],"Кошелек добавлен");
-
+                }
             }
             else{
-               $telegram->sendMessage($message['from']['id'],"Введите адрес кошелька");
+                $stats=Share::stats();
+                $stats['day']=$stats['day']/1000;
+                $stats['hour']=$stats['hour']/1000;
+
+                $text="Хешрейт за 24 часа: ".$stats['day']."GH/s\nХешрейт за 1 час: ".$stats['hour']."GH/s";
+            }
+            $telegram->sendMessage($message['from']['id'],$text);
+
+        }
+        else{
+            $farmer=Farmer::where('telegram_id',$message['from']['id'])->get()->first();
+
+            if($farmer){
+                if($message['text']=="/stats"){
+                    $stats=$farmer->stats();
+
+                    if($stats['day']>1000){
+                        $stats['day']=$stats['day']/1000;
+                        $stats['hour']=$stats['hour']/1000;
+
+                        $text="Хешрейт за 24 часа: ".$stats['day']."GH/s\nХешрейт за 1 час: ".$stats['hour']."GH/s";
+                    }
+                    else{
+                        $text="Хешрейт за 24 часа: ".$stats['day']."Mh/s\nХешрейт за 1 час: ".$stats['hour']."Mh/s";
+
+                    }
+                    $telegram->sendMessage($message['from']['id'],$text);
+                }
+                else{
+                    $telegram->sendMessage($message['from']['id'],"Я тебя не понимаю");
+                }
+            }
+            else{
+                $shares=Share::where('worker',$message['text'])->first();
+                if($shares){
+                    $farmer=new Farmer ();
+                    $farmer->telegram_id=$message['from']['id'];
+                    $farmer->address=$message['text'];
+                    $farmer->save();
+                    $telegram->sendMessage($message['from']['id'],"Кошелек добавлен");
+
+                }
+                else{
+                    $telegram->sendMessage($message['from']['id'],"Введите адрес кошелька");
+                }
             }
         }
+
     }
 }
