cfc20152cf032ce57980374666922d9186d6fd1f
sevakode <sevakode@gmail.com>
2022-01-25 02:29:16 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 });
 Route::get('/stats', [\App\Http\Controllers\BotController::class, 'statsPool']);
-Route::get('/stats/address', [\App\Http\Controllers\BotController::class, 'stats']);
+Route::get('/stats/{address}', [\App\Http\Controllers\BotController::class, 'stats']);
 
 Route::get('/balance/{address}', [\App\Http\Controllers\FarmerController::class, 'balance']);
