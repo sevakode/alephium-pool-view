767056b8ec9d33cc03815daefb48965cc73a5aaa
sevakode <sevakode@gmail.com>
2022-02-02 11:27:31 +0300

    db setting

--- config/database.php
+++ config/database.php
@@ -76,6 +76,9 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::ATTR_TIMEOUT => 600,
+            ]) : [],
         ],
 
         'sqlsrv' => [
