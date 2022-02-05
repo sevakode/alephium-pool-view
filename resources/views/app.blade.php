77b1776d7e00088837f5edcc164008596af322f5
sevakode <sevakode@gmail.com>
2022-02-05 23:56:08 +0300

    add front

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <title>YouPool</title>
+    <title>{{env('APP_NAME')}}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
